450a0f249f8eb4a9a050982fc372c244d337e28f
Ivan Liashedko <liashedkoivan@gmail.com>
2024-05-18 20:43:31 +0200

    fixed the wrong input error

--- Web/hello.py
+++ Web/hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+
 import sys
 sys.path.append('/ChemPal')
 from Final import final_solution
@@ -6,18 +7,20 @@ from Final import final_solution
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-# @app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         text = request.form.get('text')
         if text:
-            processed_text = final_solution(text)
-            return render_template('home.html', input_text=text, data=processed_text)
+            try:
+                processed_text = final_solution(text)
+                return render_template('home.html', input_text=text, data=processed_text)
+            except Exception as e:
+                app.logger.error(f"Error processing text: {e}")
+                return render_template('home.html', input_text = '',data = {'correct_input': False})
         else:
             return "No text provided", 400
     return render_template('home.html')
 
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=False)
-#  host='0.0.0.0'
+    # host='0.0.0.0'
